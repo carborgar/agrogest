41e0513127f65c5c7d0532df7a6a8021b1ac60bd
Carlos <carlosborja93@gmail.com>
2025-03-21 11:44:37 +0100

    update vercel config

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ echo "Building project packages..."
 pip install -r requirements.txt
 
 echo "Migrating database..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
 
 echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput
+python3.12 manage.py collectstatic --noinput
