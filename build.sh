97cde744c1a591fdca9d8cffe37472221d61fe8b
carborgar <carlosborja93@gmail.com>
2025-03-25 13:10:42 +0100

    añadido bootstrap personalizado y eliminado docker

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@
 echo "Building the project..."
 python3.12 -m pip install -r requirements.txt
 
+# Install Node.js dependencies and compile SCSS
+echo "Installing Node.js dependencies and compiling SCSS..."
+npm install
+# Compilar SCSS a CSS con npx
+npx sass scss/custom-bootstrap.scss static/css/bootstrap.min.css --style=compressed
+
 echo "Migrating database..."
 python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput
