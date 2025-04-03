1ba7cac17feb95905060287eb450e66cf57e6b7f
carborgar <carlosborja93@gmail.com>
2025-04-03 14:29:24 +0200

    vista de lista para tratamientos

--- farm/templatetags/custom_filters.py
+++ farm/templatetags/custom_filters.py
@@ -1,8 +1,15 @@
 from django import template
 
+from farm.models import Product
+
 register = template.Library()
 
 
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key, '')
+
+
+@register.filter
+def dose_type(type):
+    return dict(Product.ALL_DOSE_TYPE_CHOICES).get(type, '')
