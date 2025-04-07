d6d14e150e25eb2bff5bda6c198d93f90e226703
carborgar <carlosborja93@gmail.com>
2025-04-07 11:35:03 +0200

    limitación de filas en admin
    se cambia el paginado de Task y TaskProduct, que son las entidades más grandes ahora mismo para que el rendimiento no sea vea afectado (vercel)

--- farm/admin.py
+++ farm/admin.py
@@ -2,10 +2,19 @@ from django.contrib import admin
 
 from farm.models import Product, Field, Machine, Task, Harvest, TaskProduct, ProductType
 
+
+class TaskAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+
+class TaskProductAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+
 admin.site.register(Product)
 admin.site.register(Field)
 admin.site.register(Machine)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Harvest)
-admin.site.register(TaskProduct)
+admin.site.register(TaskProduct, TaskProductAdmin)
 admin.site.register(ProductType)
