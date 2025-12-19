faf3e13a1601ce18ba4dd307cdffb6b3a0d663ed
carborgar <carlosborja93@gmail.com>
2025-12-19 16:29:43 +0100

    integración con sentry para observabilidad

--- agrogest/settings.py
+++ agrogest/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 import dj_database_url
+import sentry_sdk
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -177,3 +178,23 @@ LOGGING = {
         "propagate": True,
     },
 }
+
+SENTRY_DSN = os.getenv("SENTRY_DSN")
+if not DEBUG:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        # Add data like request headers and IP for users,
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=True,
+        # Enable sending logs to Sentry
+        enable_logs=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        traces_sample_rate=1.0,
+        # Set profile_session_sample_rate to 1.0 to profile 100%
+        # of profile sessions.
+        profile_session_sample_rate=1.0,
+        # Set profile_lifecycle to "trace" to automatically
+        # run the profiler on when there is an active transaction
+        profile_lifecycle="trace",
+    )
--- agrogest/urls.py
+++ agrogest/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("farm.urls")),
     path('usuarios/', include("accounts.urls")),
+
+    path('sentry-debug/', trigger_error),
 
 ]
