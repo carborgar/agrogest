d51747660277986e0d90780d0f7642394faa91cd
carborgar <carlosborja93@gmail.com>
2026-02-19 15:04:43 +0100

    actualizada config vercel

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Install Node.js dependencies and compile SCSS
 echo "Installing Node.js dependencies and compiling SCSS..."
@@ -11,8 +11,8 @@ npm install
 npx sass scss/custom-bootstrap.scss static/css/custom-bootstrap.min.css --style=compressed
 
 echo "Migrating database..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collecting static files..."
-python3.12 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
